11a33ad2b987d088761e0f8627f36a70f1c4aee2
philip howley <howley.phil@gmail.com>
2021-03-17 17:31:18 -0400

    Add Post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// define relationships
+// User can make many posts
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+});
+
+// Post has one user
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
